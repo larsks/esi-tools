ed0a36b9441111f7c553a63f891b344810d8ec8f
Lars Kellogg-Stedman <lars@redhat.com>
2024-12-18 22:08:20 -0500

    provide some progress output in attach-all.sh

--- attach-all.sh
+++ attach-all.sh
@@ -13,7 +13,13 @@ shift $((OPTIND - 1))
 set -e
 
 for node in "$@"; do
+  # LKS: this would be a handy spot for openstack esi node network detach --all
+  echo "detach node from existing network"
   openstack esi node network detach "$node" || :
+
+  echo "attach node to \"$network\" network"
   openstack esi node network attach --network "$network" "$node"
+
+  echo "configure node to boot from disk"
   openstack baremetal node boot device set "$node" disk --persistent
 done
