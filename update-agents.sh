084fae4ad91472f9fcdd757ffed7ae5d7159a306
Lars Kellogg-Stedman <lars@redhat.com>
2024-12-20 13:14:10 -0500

    Extract row/pod/rack/etc from node name

--- update-agents.sh
+++ update-agents.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+while getopts n: ch; do
+  case $ch in
+  n) namespace=$OPTARG ;;
+  *) exit 2 ;;
+  esac
+done
+shift $((OPTIND - 1))
+
+set -eu
+
 workdir=$(mktemp -d workXXXXXX)
 trap 'rm -rf "$workdir"' EXIT
 
 echo "get list of agents"
-kubectl get agents -o json |
+kubectl ${namespace:+-n "$namespace"} get agents -o json |
   jq -r '.items[]|[.metadata.name, (.status.inventory.interfaces[]|select(.flags|index("running")).macAddress)]|@tsv' >"$workdir/agents.tab"
 
 while read -r agent macaddr; do
@@ -20,10 +30,20 @@ while read -r agent macaddr; do
     continue
   fi
 
+  if [[ $node =~ MOC-R([0-9]+)P([A-Z])C([0-9]+)U([0-9]+)-S(.*) ]]; then
+    echo "labelling node $node with: ${BASH_REMATCH[*]}"
+    kubectl ${namespace:+-n "$namespace"} label agent "$agent" \
+      topology.nerc.mghpcc.org/row="${BASH_REMATCH[1]}" \
+      topology.nerc.mghpcc.org/pod="${BASH_REMATCH[2]}" \
+      topology.nerc.mghpcc.org/cabinet="${BASH_REMATCH[3]}" \
+      topology.nerc.mghpcc.org/u="${BASH_REMATCH[4]}" \
+      topology.nerc.mghpcc.org/slot="${BASH_REMATCH[5]}"
+  fi
+
   # ensure node name is lower case
   node=${node,,}
   echo "found node $node"
-  kubectl patch agent "$agent" --type json --patch-file /dev/stdin <<EOF
+  kubectl ${namespace:+-n "$namespace"} patch agent "$agent" --type json --patch-file /dev/stdin <<EOF
 [
 {"op": "add", "path": "/spec/hostname", "value": "$node"}
 ]
